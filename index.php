e65e8944f622bae0d9188a58d5a609526c7e56a0
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2017-04-16 20:18:11 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -7,20 +7,29 @@
   
   <title>Kalkulator</title>
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/kalkulator.css" rel="stylesheet">
 </head>
-<body>
+<body class>
 
-<form action="" method="POST">
-	<input type="text" name="liczba1"/>
-	<select name="dzialanie">
-		<option value="+">+</option>
-		<option value="-">-</option>
-		<option value="*">*</option>
-		<option value="/">/</option>
-		</select>
-	<input type="text" name="liczba2"/>
-	<input type="submit" value='Oblicz' name="OBLICZ">
-</form>
+<div class="container">
+<div class="header">
+	<div class="jumbotron">
+		<h1>Prosty kalkulator w PHP</h1>
+	</div>
+</div>
+	<div class="row">
+		<div class="lg-col-12 sm-col-12">
+			<form action="" method="POST">
+				<input type="text" name="liczba1"/>
+				<select name="dzialanie">
+					<option value="+">+</option>
+					<option value="-">-</option>
+					<option value="*">*</option>
+					<option value="/">/</option>
+					</select>
+				<input type="text" name="liczba2"/>
+				<input type="submit" value='Oblicz' name="OBLICZ">
+			</form>
 
 <?php
 require_once './classes/Kalkulator.php';
@@ -31,4 +40,8 @@ $dzialanie = $_POST['dzialanie'];
 $kalk = new Kalkulator($liczba1, $liczba2, $dzialanie);
 echo $kalk->oblicz();
 ?>
+
+		</div>
+	</div>
+</div>
 </body>
