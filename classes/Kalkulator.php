56a453299fcda2b19dbced2907dcbb014bdcb2e8
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2017-04-18 18:47:33 +0200

    ref

--- classes/Kalkulator.php
+++ classes/Kalkulator.php
@@ -5,30 +5,33 @@ class Kalkulator {
 	public $num1;
 	public $num2;
 	public $dzialanie;
-	public $errorInputData = 0;
-	private $formulka;
+	public $errorInputData = false;
+	public $formulka;
 	
 	public function __construct($liczba1POST, $liczba2POST, $dzialaniePOST) {
-	
-	if(is_numeric($liczba1POST)){
-		$this->num1 = $liczba1POST;
-		} else {
-			$errorInputData = 1;
-			$this->num1 = 0;
-		}
-	if(is_numeric($liczba2POST)){
-			$this->num2 = $liczba2POST;
-		} else {
-			$errorInputData = 1;
-			$this->num2 = 0;
-		}
+	$this->wskaznik1 = is_numeric($liczba1POST);
+	$this->wskaznik2 = is_numeric($liczba2POST);
+
+	$this->num1 = $liczba1POST;
+	$this->num2 = $liczba2POST;
+
+	if(is_numeric($liczba1POST)== false){
+			$this->errorInputData = true;
+			$this->eid = $errorInputData;
+		} 
+	if(is_numeric($liczba2POST)== false){
+			$this->errorInputData = true;		
+			$this->eid = $errorInputData;
+		} 
 	$this->dzialanie = $dzialaniePOST;
 	}
 
 	public function odpowiedz($pierwszaLiczba, $drugaLiczba, $dzialanieNaLiczbach, $errorInput){
 	
-		if($errorInput > 0){
-			$this->formulka = '<h1>Jedna z podanych wartosci nie jest liczbą. Liczba 1: '.$pierwszaLiczba.' Liczba 2: '.$drugaLiczba.'</h1>';
+		if($errorInput == true){
+			$this->formulka = '<h1>Jedna z podanych wartosci nie jest liczbą.</br> Wartosc 1: '
+			.$pierwszaLiczba.' Wartosc 2: '.$drugaLiczba.'</h1>';
+			$this->errorInputData = true;
 		} else {
 			$this->formulka = '<h1>Wynik działania '.$pierwszaLiczba.' '.$dzialanieNaLiczbach.' '.$drugaLiczba.' to :'; 
 		}
@@ -39,22 +42,25 @@ class Kalkulator {
 		$wynikDzialania;
 		switch ($this->dzialanie) {
 			case '+' :
-				//echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
+				
 				$wynikDzialania =  $this->odpowiedz($this->num1, $this->num2, $this->dzialanie, $this->errorInputData);
+				$wynikDzialania .= $this->num1 + $this->num2;
 				return $wynikDzialania;
-				//return $this->num1 + $this->num2;
 				break;
 			case '-' :
-				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
-				return  $this->num1 - $this->num2;
+				$wynikDzialania =  $this->odpowiedz($this->num1, $this->num2, $this->dzialanie, $this->errorInputData);
+				$wynikDzialania .= $this->num1 - $this->num2;
+				return $wynikDzialania;
 				break;
 			case '*' :
-				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
-				return  $this->num1 * $this->num2;
+				$wynikDzialania =  $this->odpowiedz($this->num1, $this->num2, $this->dzialanie, $this->errorInputData);
+				$wynikDzialania .= $this->num1 * $this->num2;
+				return $wynikDzialania;
 				break;
 			case '/' :
-				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
-				return  $this->num1 / $this->num2;
+				$wynikDzialania =  $this->odpowiedz($this->num1, $this->num2, $this->dzialanie, $this->errorInputData);
+				$wynikDzialania .= $this->num1 / $this->num2;
+				return $wynikDzialania;
 				break;
 			default :
 				echo '<h1>Podaj dwie liczby i naciśnij przycisk OBLICZ</h1>';
