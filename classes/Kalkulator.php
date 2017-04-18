c1043ec925dfa84976386f8b8f7af7ebc89f2de6
Mariusz Brzostek <mariusz.brzostek@gmail.com>
2017-04-18 02:05:30 +0200

    refactor

--- classes/Kalkulator.php
+++ classes/Kalkulator.php
@@ -5,35 +5,62 @@ class Kalkulator {
 	public $num1;
 	public $num2;
 	public $dzialanie;
+	public $errorInputData = 0;
+	private $formulka;
 	
 	public function __construct($liczba1POST, $liczba2POST, $dzialaniePOST) {
-		
-	$this->num1 = $liczba1POST;
-	$this->num2 = $liczba2POST;
+	
+	if(is_numeric($liczba1POST)){
+		$this->num1 = $liczba1POST;
+		} else {
+			$errorInputData = 1;
+			$this->num1 = 0;
+		}
+	if(is_numeric($liczba2POST)){
+			$this->num2 = $liczba2POST;
+		} else {
+			$errorInputData = 1;
+			$this->num2 = 0;
+		}
 	$this->dzialanie = $dzialaniePOST;
 	}
+
+	public function odpowiedz($pierwszaLiczba, $drugaLiczba, $dzialanieNaLiczbach, $errorInput){
 	
+		if($errorInput > 0){
+			$this->formulka = '<h1>Jedna z podanych wartosci nie jest liczbą. Liczba 1: '.$pierwszaLiczba.' Liczba 2: '.$drugaLiczba.'</h1>';
+		} else {
+			$this->formulka = '<h1>Wynik działania '.$pierwszaLiczba.' '.$dzialanieNaLiczbach.' '.$drugaLiczba.' to :'; 
+		}
+		return $this->formulka;
+	}
+
 	public function oblicz() {
+		$wynikDzialania;
 		switch ($this->dzialanie) {
 			case '+' :
-				echo 'Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
-				return $this->num1 + $this->num2;
+				//echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
+				$wynikDzialania =  $this->odpowiedz($this->num1, $this->num2, $this->dzialanie, $this->errorInputData);
+				return $wynikDzialania;
+				//return $this->num1 + $this->num2;
 				break;
 			case '-' :
-				echo 'Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
+				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
 				return  $this->num1 - $this->num2;
 				break;
 			case '*' :
-				echo 'Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
+				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
 				return  $this->num1 * $this->num2;
 				break;
 			case '/' :
-				echo 'Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
+				echo '<h1>Wynik działania '.$this->num1.' '.$this->dzialanie.' '.$this->num2.' to: ';
 				return  $this->num1 / $this->num2;
 				break;
 			default :
-				echo 'Podaj dwie liczby i naciśnij przycisk OBLICZ';
+				echo '<h1>Podaj dwie liczby i naciśnij przycisk OBLICZ</h1>';
 				break;
 		}
 	}
+
+	
 }
--- index.php
+++ index.php
@@ -6,41 +6,54 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   
   <title>Kalkulator</title>
-  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/kalkulator.css" rel="stylesheet">
-</head>
-<body class>
+  	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  	<link href="css/kalkulator.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato|Roboto&amp;subset=latin-ext" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	<script type="text/javascript" src="assets/js/lib/jquery-3.1.1.min.js"></script>
 
-<div class="container">
+</head>
+<body>
+
+<div class="container wyrownanie">
 <div class="header">
 	<div class="jumbotron">
-		<h1>Prosty kalkulator w PHP</h1>
+		<h1>Prosty kalkulator w PHP <i class="fa fa-calculator" aria-hidden="true"></i></h1>
 	</div>
 </div>
 	<div class="row">
-		<div class="lg-col-12 sm-col-12">
+		<div class="col-lg-12 col-sm-12">
+			<div class="">
 			<form action="" method="POST">
-				<input type="text" name="liczba1"/>
-				<select name="dzialanie">
-					<option value="+">+</option>
-					<option value="-">-</option>
-					<option value="*">*</option>
-					<option value="/">/</option>
+				<div class="form-group">
+					<input class="form-control" type="text" name="liczba1"/>
+				</div>
+				<div class="form-group">
+					<select class="form-control" name="dzialanie">
+						<option value="+">+</option>
+						<option value="-">-</option>
+						<option value="*">*</option>
+						<option value="/">/</option>
 					</select>
-				<input type="text" name="liczba2"/>
-				<input type="submit" value='Oblicz' name="OBLICZ">
+				</div>
+				<div class="form-group">	
+					<input class="form-control" type="text" name="liczba2"/>
+				</div>
+				<div class="form-group">
+					<input class="btn btn-lg btn-info" type="submit" value='Oblicz' name="OBLICZ">
+				</div>
 			</form>
 
-<?php
-require_once './classes/Kalkulator.php';
+			<?php
+			require_once './classes/Kalkulator.php';
 
-$liczba1 = $_POST['liczba1'];
-$liczba2 = $_POST['liczba2'];
-$dzialanie = $_POST['dzialanie'];
-$kalk = new Kalkulator($liczba1, $liczba2, $dzialanie);
-echo $kalk->oblicz();
-?>
-
+			$liczba1 = $_POST['liczba1'];
+			$liczba2 = $_POST['liczba2'];
+			$dzialanie = $_POST['dzialanie'];
+			$kalk = new Kalkulator($liczba1, $liczba2, $dzialanie);
+			echo $kalk->oblicz().'</h1>';
+			?>
+			</div>
 		</div>
 	</div>
 </div>
